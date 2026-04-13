8662ff1d99a173ffb89e34a0799e5fa31bf83e66
Zub Air <ersenoy@gmail.com>
2026-04-13 18:24:53 +0400

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+# start.py
+import threading
+from bot.handlers import run_bot
+from webserver import app
+
+if __name__ == "__main__":
+    # Запускаем бота в отдельном потоке
+    bot_thread = threading.Thread(target=run_bot, daemon=True)
+    bot_thread.start()
+    
+    # Запускаем Flask-сервер в основном потоке
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
