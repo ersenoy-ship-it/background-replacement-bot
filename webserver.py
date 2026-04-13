98571f44a68ff653c95b63fa8be35cd4ccc59e08
Zub Air <ersenoy@gmail.com>
2026-04-13 18:21:08 +0400

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,13 @@
+# webserver.py
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "Bot is running!", 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
